41013784c7e0575173b81c00bb04ea12f9893be5
Cecilia Morlacchi <90002130+CeciMorla@users.noreply.github.com>
2022-01-29 16:39:16 -0300

    Update index.js

--- client/src/reducer/index.js
+++ client/src/reducer/index.js
@@ -1,16 +1,45 @@
-import { ALL_TEAMS } from "../actions/constantes";
+import { FIXTURE_LIVE, FIXTURE_LAST, FIXTURE_NEXT, STATITICS, HEAD_TO_HEAD, EVENT } from "../actions/constantes";
 
 const initialState = {
-    teams: [],
-    
+    live: [],
+    last:[],
+    next:[],
+    statitics:[],
+    head: [],
+    event: []
 };
 
 const rootReducer = (state = initialState, action) =>{
     switch(action.type){
-        case ALL_TEAMS:
+        case FIXTURE_LIVE:
             return{
                 ...state,
-                teams : action.payload
+                live : action.payload
+            }
+        case FIXTURE_LAST:
+            return{
+                ...state,
+                last: action.payload
+            }
+        case FIXTURE_NEXT:
+            return{
+                ...state,
+                next: action.payload
+            }
+        case STATITICS:
+            return{
+                ...state,
+                statitics: action.payload
+            }
+        case HEAD_TO_HEAD:
+            return{
+                ...state,
+                head: action.payload
+            }
+        case EVENT:
+            return{
+                ...state,
+                event: action.payload
             }
         default:
             return state;

6a6c4e1cbf1aeccb8bdb5553afe26cea70fbd683
Cecilia Morlacchi <90002130+CeciMorla@users.noreply.github.com>
2022-01-29 16:34:33 -0300

    Update constantes.js

--- client/src/actions/constantes.js
+++ client/src/actions/constantes.js
@@ -1,1 +1,6 @@
-export const ALL_TEAMS = 'ALL_TEAMS';
+export const FIXTURE_LIVE = 'FIXTURE_LIVE';
+export const FIXTURE_LAST = 'FIXTURE_LAST';
+export const FIXTURE_NEXT = 'FIXTURE_NEXT';
+export const STATITICS = 'STATITICS';
+export const HEAD_TO_HEAD = 'HEAD_TO_HEAD';
+export const EVENT = 'EVENT';

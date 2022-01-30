5afb02db4095f0d115704436f3fdc0084552c0a9
Cecilia Morlacchi <90002130+CeciMorla@users.noreply.github.com>
2022-01-30 13:26:36 -0300

    update

--- client/src/components/Event/Event.js
+++ client/src/components/Event/Event.js
@@ -1,7 +1,16 @@
 import React from "react";
 
-const Event = () =>{
-
+const Event = ({name,logo,playerName,assistName,type,detail}) =>{
+    return (
+        <div>
+            <img src={logo} alt='img'/>
+            <h2>{name}</h2>
+            <h3>{playerName}</h3>
+            <p>{assistName}</p>
+            <p>{type}</p>
+            <p>{detail}</p>
+        </div>
+    )
 }
 
 export default Event;
--- client/src/components/Statistics/Statistics.js
+++ client/src/components/Statistics/Statistics.js
@@ -1,26 +1,28 @@
 import React, {useEffect} from "react";
 import { useDispatch, useSelector } from "react-redux";
 import Nav from "../Nav/Nav";
-import {statistics} from '../../actions/index.js';
+import {statistics, event} from '../../actions/index.js';
 import TableStatistics from "../TableStatistics/TableStatistics";
 import { useParams } from "react-router-dom";
-import TableLive from "../TableLive/TableLive";
+import Event from "../Event/Event";
 
 const Statistics = () => {
     const dispatch = useDispatch();
     const statistic = useSelector((state)=> state.statistics);
+    const even = useSelector((state)=> state.event)
     let {fixture} = useParams();
     console.log(statistic)
     
     useEffect(()=>{
         dispatch(statistics(fixture))
+        dispatch(event(fixture))
     },[])
 
     return(
         <div>
             <Nav/>
             {
-                statistic?.map((e,i) => <TableStatistics
+                statistic?.map((e) => <TableStatistics
                             key = {e.team.id}
                             name = {e.team.name}
                             logo = {e.team.logo}
@@ -28,6 +30,18 @@ const Statistics = () => {
                             value = {e.statistics.map(e => e.value)}
                 />)
             }
+            {
+                even?.map((e)=> <Event
+                        key = {e.team.name}
+                        name = {e.team.name}
+                        logo = {e.team.logo}
+                        playerName={e.player.name}
+                        assistName={e.assist.name}
+                        type={e.type}
+                        detail={e.detail}
+                />)
+            }
+
         </div>
     )
 }

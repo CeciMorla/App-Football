eefd1e0ddd026e282ceb359f641769cb15126713
Cecilia Morlacchi <90002130+CeciMorla@users.noreply.github.com>
2022-02-01 10:38:30 -0300

    update

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -1,22 +1,25 @@
 import React from "react";
 import {Link} from 'react-router-dom';
 import img from './img/2.png';
+import s from './Nav.module.css';
 
 const Nav = ()=>{
     return(
-        <div>
+        <div className={s.nav}>
             <Link to='/'>
-            <img src={img} alt="img"/>
+            <img src={img} alt="img" className={s.img}/>
             </Link>
+            <div className="btn-group" role="group" aria-label="Basic example" className={s.btn}>
             <Link to='/live'>
-                <button>LIVE</button>
+                <button type="button" class="btn btn-light">LIVE</button>
             </Link>
             <Link to='/next'>
-                <button>NEXT GAME</button>
+                <button type="button" class="btn btn-light">NEXT GAME</button>
             </Link>
             <Link to='/finish'>
-                <button>FINISH GAME</button>
+                <button type="button" class="btn btn-light">FINISH GAME</button>
             </Link>
+            </div>
         </div>
     )
 }
